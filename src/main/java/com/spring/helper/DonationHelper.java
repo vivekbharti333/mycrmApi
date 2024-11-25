1c697f7e099507f278326c76a2789bae4ae85c04
Vivek Bharti (NIC) <vivekbharti@nic.in>
2024-11-25 17:31:41 +0530

    clean

--- src/main/java/com/spring/helper/DonationHelper.java
+++ src/main/java/com/spring/helper/DonationHelper.java
@@ -386,7 +386,7 @@ public DonationDetails getUpdatedDonationDetailsByReqObj(DonationRequestObject d
 		Object[] count = new Object[] {};
 		if (donationRequest.getRoleType().equals(RoleType.SUPERADMIN.name())) {
 			count = (Object[]) donationDetailsDao.getEntityManager().createQuery(
-					"SELECT COUNT(id) AS count, SUM(amount) AS amount FROM DonationDetails DD where DD.createdAt BETWEEN :firstDate AND :lastDate AND DD.superadminId = :superadminId AND DD.status =:status")
+					"SELECT COUNT(id) AS count, SUM(amount) AS amount FROM DonationDetails DD where DD.createdAt BETWEEN :firstDate AND :lastDate AND DD.superadminId = :superadminId AND DD.status =:status GROUP BY DD.currencyCode")
 					.setParameter("firstDate", firstDate, TemporalType.DATE)
 					.setParameter("lastDate", secondDate, TemporalType.DATE)
 					.setParameter("superadminId", donationRequest.getSuperadminId())
@@ -405,7 +405,7 @@ public DonationDetails getUpdatedDonationDetailsByReqObj(DonationRequestObject d
 			return count;
 		} else {
 			count = (Object[]) donationDetailsDao.getEntityManager().createQuery(
-					"SELECT COUNT(id) AS count, SUM(amount) AS amount FROM DonationDetails DD where DD.createdAt BETWEEN :firstDate AND :lastDate AND DD.superadminId = :superadminId AND DD.createdBy =:createdBy AND DD.status =:status ")
+					"SELECT COUNT(id) AS count, SUM(amount) AS amount FROM DonationDetails DD where DD.createdAt BETWEEN :firstDate AND :lastDate AND DD.superadminId = :superadminId AND DD.createdBy =:createdBy AND DD.status =:status")
 					.setParameter("firstDate", firstDate, TemporalType.DATE)
 					.setParameter("lastDate", secondDate, TemporalType.DATE)
 					.setParameter("superadminId", donationRequest.getSuperadminId())
